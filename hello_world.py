5e12f40ebf143c91a48f7e6667bd62619b968dc9
andisiwememela102-ux <andisiwememela102@gmail.com>
2026-06-11 18:47:11 +0200

    Hello world Python script
    Created hello_world.py file with a simple program that prints "Hello, World!" to the console.

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+
+#promp the user for their name
+name = input("What is your name? ")
+#say hello to the user
+print("Hello, " + name + "!")
+#print("This is a simple program that prompts the user for their name and then greets them.")
+name ="Andy"
+print("Hello, " + name + "!")
+
+#creating and using variables
+age = 21
+print("You are " + str(age) + " years old.")
+
+
+print("Hello,World!")
+
+
