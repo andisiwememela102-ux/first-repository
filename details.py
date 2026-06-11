f3b29b507e871dba8fdc5e337598626711dfcb74
andisiwememela102-ux <andisiwememela102@gmail.com>
2026-06-11 23:21:05 +0200

    Collected and displayed user's details
    and implemented a print statement that combines all user details into a single descriptive sentence.

---
+++ details.py
@@ -0,0 +1,30 @@
+#pseudo code:
+#1. Ask the user for their name and store it in a variable called name.
+# 2. Ask the user for their age and store it in a variable called age.
+# 3. Ask the user for their house number and store it in a variable called house_number.
+# 4. Ask the user for their street name and store it in a variable called street_name.   
+# 5. Display the users details in a formatted string that includes their name, age, and address.
+
+
+
+
+# This program collects and displays the users details, including their name, age, and address.
+
+# Ask the user for their name
+name = input("Please enter your name: ")
+name= "Andisiwe Memela"
+
+# Ask the user for their age
+age = input("Please enter your age: ")
+age = 21
+
+# Ask the user for their HOUSE NUMBER 
+house_number = input("Please enter your house number: ")
+house_number = 145
+
+# Ask the user for their street name
+street_name = input("Please enter your street name: ")
+street_name = "Stellenbosch street"
+
+# Display the user's details
+print(f"This is {name}, she is  {age} years old and lives at house number {house_number} on {street_name}.")
