3dbbc8a27c4d9a9039e241bbdac04967648a35d7
andisiwememela102-ux <andisiwememela102@gmail.com>
2026-06-12 21:30:35 +0200

    Replaced and Added sentences in reverse.

---
+++ replace.py
@@ -0,0 +1,19 @@
+
+#Original sentence with exclamation mark
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog!"
+
+#Replace exclamation marks with spaces
+new_sentence = sentence.replace("!", " ")
+
+#convert to upper case
+new_sentence = new_sentence.upper()
+
+#Print the sentence to upper case
+print(new_sentence)
+
+#Reverse the sentence
+reversed_sentence = new_sentence[::-1]
+
+#Print the reversed sentence
+print(reversed_sentence)
+
