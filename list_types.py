84bd6ce3a8818cabdb6aac61b3f2c02465fe4ba2
andisiwememela102-ux <andisiwememela102@gmail.com>
2026-06-15 23:22:49 +0200

    Created a list_types.py for tracking friend names

---
+++ list_types.py
@@ -0,0 +1,19 @@
+# Enter a list of your friends names
+friends_names = ['Andy', 'Stacey', 'Rose']
+
+# Print the first friend's name
+print(f"first friend:{friends_names[0]}")
+
+# Print the last friend's name
+print(f"last friend:{friends_names[2]}")
+
+#print the length of list
+print(f"length of the list:{len(friends_names)}")
+
+# Define your friend's age
+friends_age = [21, 23, 26]
+
+# Print each friend's name and age in order
+print(f"{friends_names[0]} is {friends_age[0]} years old.")
+print(f"{friends_names[1]} is {friends_age[1]} years old.")
+print(f"{friends_names[2]} is {friends_age[2]} years old.")
